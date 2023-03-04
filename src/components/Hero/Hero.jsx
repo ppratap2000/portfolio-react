6ef99d890527c28cd1b472c0a4ad982019cdb563
Pranav Pratap <111981133+ppratap2000@users.noreply.github.com>
2023-03-05 00:30:41 +0530

    Update Hero.js

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -47,8 +47,8 @@ const Hero = () => {
 
                     <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
                         <img src="./certificate.png" alt="" />
-                        <span>CERTIFIED PROFATIONAL</span>
-                        <span>UI/UX DESIGNER</span>
+                        <span>CERTIFIED PROFESSIONAL</span>
+                        <span>FullStack Developer</span>
                     </motion.div>
                 </div>
             </motion.div>
